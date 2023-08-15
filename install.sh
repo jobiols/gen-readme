c1eaf64436aba912556fee91196ff74b8f10067d
Jorge Obiols <jorge.obiols@gmail.com>
2023-08-15 19:58:33 -0300

    [IMP] Crear readme file v1.1.0
    - Si no existe el directorio readme lo crea con todos los fragmentos
    - Si la cantidad de caracteres es menor que 1000 no genera la tabla
      de contenidos
    - Algunas mejoras en documentacion

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# instalador de gen_readme
 python3 -m pip install --user -e .
--- tools/__main__.py
+++ tools/__main__.py
@@ -1,4 +1,4 @@
-from gen_readme import gen_readme
+from . import gen_readme
 
 if __name__ == "__main__":
-    raise SystemExit(gen_readme())
+    raise SystemExit(gen_readme.gen_readme())
