d52c0c1abb49659152eac8d1bbdf4fb08f220677
Jorge Obiols <jorge.obiols@gmail.com>
2024-02-11 13:57:52 -0300

    [FIX] jinja vulnerability bump from 3.1.2 to 3.1.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     data_files=[("mypackage", ["tools/gen_addon_readme.template"])],
     install_requires=[
         "click==8.1.7",
-        "jinja2==3.1.2",
+        "jinja2==3.1.3",
         "docutils==0.20.1",
         "pigments==1.6",
     ],
